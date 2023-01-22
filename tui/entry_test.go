f267769c442b67abc493193f1176d7f3103df26a
DJ Yuhn <djyuhn@gmail.com>
2023-01-22 15:30:14 -0600

    Create BubbleTea Tui entry

---
+++ tui/entry_test.go
@@ -0,0 +1,49 @@
+package tui_test
+
+import (
+	"testing"
+
+	"gitcha/tui"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEntryModel_Init(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return nil command", func(t *testing.T) {
+		t.Parallel()
+		model := tui.EntryModel{}
+
+		actual := model.Init()
+
+		assert.Nil(t, actual)
+	})
+}
+
+func TestEntryModel_Update(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return same model and nil msg", func(t *testing.T) {
+		t.Parallel()
+		expectedModel := tui.EntryModel{}
+
+		model, cmd := expectedModel.Update(nil)
+
+		assert.Equal(t, expectedModel, model)
+		assert.Nil(t, cmd)
+	})
+}
+
+func TestEntryModel_View(t *testing.T) {
+	t.Parallel()
+
+	t.Run("should return Entry View string", func(t *testing.T) {
+		t.Parallel()
+		model := tui.EntryModel{}
+
+		actual := model.View()
+
+		assert.Equal(t, "Entry View", actual)
+	})
+}
