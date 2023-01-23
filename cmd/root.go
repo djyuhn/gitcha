f3187f6c3d149f9c1d1ea1ccd83df7fef56536cf
DJ Yuhn <djyuhn@gmail.com>
2023-01-23 16:13:10 -0600

    Wire up cmd layer to call App's GitchaTui.

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"os"
 
+	"gitcha/cmd/gitcha"
+	"gitcha/tui"
+
 	"github.com/spf13/cobra"
 )
 
@@ -13,9 +16,22 @@ type RootCmd struct {
 func NewRootCmd() RootCmd {
 	return RootCmd{
 		Command: cobra.Command{
-			Use:   "gitcha",
-			Short: "A command-line tool to get Git information.",
-			Long:  "Gitcha is a Git CLI tool to get Git information for repositories.",
+			Use:     "gitcha",
+			Short:   "A command-line tool to get Git information.",
+			Long:    "Gitcha is a Git CLI tool to get Git information for repositories.",
+			Example: "gitcha",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				app, err := gitcha.NewApp(tui.EntryModel{})
+				if err != nil {
+					return err
+				}
+
+				if err := app.GitchaTui(); err != nil {
+					return err
+				}
+
+				return nil
+			},
 		},
 	}
 }
