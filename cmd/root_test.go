56373182bc7214f7208f0e0ca35f6428ded46398
DJ Yuhn <djyuhn@gmail.com>
2023-01-23 16:13:10 -0600

    Wire up cmd layer to call App's GitchaTui.

--- cmd/root_test.go
+++ cmd/root_test.go
@@ -17,14 +17,18 @@ func TestNewRootCmd(t *testing.T) {
 
 		expected := cmd.RootCmd{
 			Command: cobra.Command{
-				Use:   "gitcha",
-				Short: "A command-line tool to get Git information.",
-				Long:  "Gitcha is a Git CLI tool to get Git information for repositories.",
+				Use:     "gitcha",
+				Short:   "A command-line tool to get Git information.",
+				Long:    "Gitcha is a Git CLI tool to get Git information for repositories.",
+				Example: "gitcha",
 			},
 		}
 
 		actual := cmd.NewRootCmd()
 
-		assert.Equal(t, expected, actual)
+		assert.Equal(t, expected.Command.Use, actual.Use)
+		assert.Equal(t, expected.Command.Short, actual.Short)
+		assert.Equal(t, expected.Command.Long, actual.Long)
+		assert.Equal(t, expected.Command.Example, actual.Example)
 	})
 }
