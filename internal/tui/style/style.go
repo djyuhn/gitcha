08c0dfc897247de90f1344b13de9c39614c403a2
DJ Yuhn <djyuhn@gmail.com>
2023-03-15 09:45:18 -0500

    Define default themes and allow for config theme.
    This commit introduces the catppuccin theme to be used as default
    themes.

---
+++ internal/tui/style/style.go
@@ -0,0 +1,47 @@
+package style
+
+import (
+	catppuccin "github.com/catppuccin/go"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type ThemeGeneral struct {
+	BaseColor      lipgloss.AdaptiveColor
+	PrimaryColor   lipgloss.AdaptiveColor
+	SecondaryColor lipgloss.AdaptiveColor
+}
+
+type ThemeConfig struct {
+	General ThemeGeneral
+}
+
+type Theme struct {
+	General ThemeGeneral
+}
+
+func NewTheme(cfg *ThemeConfig) *Theme {
+	if cfg == nil {
+		defaultTheme := &Theme{
+			General: ThemeGeneral{
+				BaseColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Base().Hex,
+					Dark:  catppuccin.Mocha.Base().Hex,
+				},
+				PrimaryColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Sapphire().Hex,
+					Dark:  catppuccin.Mocha.Sapphire().Hex,
+				},
+				SecondaryColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Rosewater().Hex,
+					Dark:  catppuccin.Mocha.Rosewater().Hex,
+				},
+			},
+		}
+		return defaultTheme
+	}
+
+	configTheme := &Theme{
+		General: cfg.General,
+	}
+	return configTheme
+}
---
+++ internal/tui/style/style_test.go
@@ -0,0 +1,48 @@
+package style_test
+
+import (
+	"testing"
+
+	catppuccin "github.com/catppuccin/go"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/djyuhn/gitcha/internal/tui/style"
+)
+
+func TestNewTheme(t *testing.T) {
+	t.Run("given nil theme config should return default general colors", func(t *testing.T) {
+		expected := &style.Theme{
+			General: style.ThemeGeneral{
+				BaseColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Base().Hex,
+					Dark:  catppuccin.Mocha.Base().Hex,
+				},
+				PrimaryColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Sapphire().Hex,
+					Dark:  catppuccin.Mocha.Sapphire().Hex,
+				},
+				SecondaryColor: lipgloss.AdaptiveColor{
+					Light: catppuccin.Latte.Rosewater().Hex,
+					Dark:  catppuccin.Mocha.Rosewater().Hex,
+				},
+			},
+		}
+
+		actual := style.NewTheme(nil)
+
+		assert.Equal(t, expected.General, actual.General)
+	})
+
+	t.Run("given theme config with general values should return theme with theme config general values", func(t *testing.T) {
+		cfg := &style.ThemeConfig{General: style.ThemeGeneral{
+			BaseColor:      lipgloss.AdaptiveColor{Light: "#FFFFFF", Dark: "#000000"},
+			PrimaryColor:   lipgloss.AdaptiveColor{Light: "#123456", Dark: "#abcdef"},
+			SecondaryColor: lipgloss.AdaptiveColor{Light: "#987654", Dark: "#fedcba"},
+		}}
+
+		actual := style.NewTheme(cfg)
+
+		assert.Equal(t, cfg.General, actual.General)
+	})
+}
