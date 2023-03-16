79a800ea20cee46c01a1dbfa5c69339a83baee96
DJ Yuhn <djyuhn@gmail.com>
2023-03-16 13:16:44 -0500

    Refactor Overview to use theme to display repo details

--- internal/tui/overview/overview.go
+++ internal/tui/overview/overview.go
@@ -7,14 +7,17 @@ import (
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 
 	"github.com/djyuhn/gitcha/internal/reporeader"
+	"github.com/djyuhn/gitcha/internal/tui/style"
 )
 
 const topAuthorCount = 3
 
 type Overview struct {
 	RepoDetails reporeader.RepoDetails
+	theme       style.Theme
 
 	orderedAuthorsByCommitCount []AuthorCommitsPair
 }
@@ -24,7 +27,9 @@ var _ tea.Model = Overview{}
 func NewOverview(repoDetails reporeader.RepoDetails) Overview {
 	topAuthorsByCommits := getSortedAuthorsByCommitCount(repoDetails.AuthorsCommits)
 
-	return Overview{RepoDetails: repoDetails, orderedAuthorsByCommitCount: topAuthorsByCommits}
+	defaultTheme := style.NewDefaultTheme()
+
+	return Overview{RepoDetails: repoDetails, orderedAuthorsByCommitCount: topAuthorsByCommits, theme: *defaultTheme}
 }
 
 func (o Overview) Init() tea.Cmd {
@@ -37,19 +42,61 @@ func (o Overview) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (o Overview) View() string {
 	view := strings.Builder{}
-	view.WriteString(fmt.Sprintf("Repository Created Date - %s\n", o.RepoDetails.CreatedDate.Format(time.RFC822)))
-	view.WriteString(fmt.Sprintf("Repository License - %s\n", o.RepoDetails.License))
-	if len(o.orderedAuthorsByCommitCount) < topAuthorCount {
-		for _, pair := range o.orderedAuthorsByCommitCount {
-			view.WriteString(fmt.Sprintf("AuthorEmail - %s : Email - %s : Commit count - %d\n", pair.AuthorName, pair.AuthorEmail, len(pair.Commits)))
-		}
-	} else {
-		for i := 0; i < topAuthorCount; i++ {
-			name := o.orderedAuthorsByCommitCount[i].AuthorName
-			email := o.orderedAuthorsByCommitCount[i].AuthorEmail
-			count := len(o.orderedAuthorsByCommitCount[i].Commits)
-			view.WriteString(fmt.Sprintf("AuthorEmail - %s : Email - %s : Commit count - %d\n", name, email, count))
-		}
+
+	view.WriteString(o.buildRepoCreatedDateView() + "\n")
+	view.WriteString(o.buildLicenseView() + "\n")
+	view.WriteString(o.buildAuthorView() + "\n")
+
+	return view.String()
+}
+
+func (o Overview) buildRepoCreatedDateView() string {
+	view := strings.Builder{}
+
+	primaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.PrimaryColor)
+	secondaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.SecondaryColor)
+
+	labelView := primaryColorStyle.Render("Created:")
+	createdDateView := secondaryColorStyle.Render(o.RepoDetails.CreatedDate.Format(time.RFC822))
+
+	view.WriteString(fmt.Sprintf("%s %s", labelView, createdDateView))
+
+	return view.String()
+}
+
+func (o Overview) buildLicenseView() string {
+	view := strings.Builder{}
+
+	primaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.PrimaryColor)
+	secondaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.SecondaryColor)
+
+	labelView := primaryColorStyle.Render("License:")
+	licenseView := secondaryColorStyle.Render(o.RepoDetails.License)
+
+	view.WriteString(fmt.Sprintf("%s %s", labelView, licenseView))
+
+	return view.String()
+}
+
+func (o Overview) buildAuthorView() string {
+	view := strings.Builder{}
+
+	authorCount := topAuthorCount
+
+	if len(o.orderedAuthorsByCommitCount) < authorCount {
+		authorCount = len(o.orderedAuthorsByCommitCount)
+	}
+
+	for i := 0; i < authorCount; i++ {
+		primaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.PrimaryColor)
+		secondaryColorStyle := lipgloss.NewStyle().Foreground(o.theme.General.SecondaryColor)
+
+		label := primaryColorStyle.Render("Author:")
+		name := secondaryColorStyle.Render(o.orderedAuthorsByCommitCount[i].AuthorName)
+		email := secondaryColorStyle.Render(o.orderedAuthorsByCommitCount[i].AuthorEmail)
+		count := secondaryColorStyle.Render(fmt.Sprintf("%d", len(o.orderedAuthorsByCommitCount[i].Commits)))
+
+		view.WriteString(fmt.Sprintf("%s %s %s %s\n", label, name, email, count))
 	}
 
 	return view.String()
--- internal/tui/style/style.go
+++ internal/tui/style/style.go
@@ -21,27 +21,31 @@ type Theme struct {
 
 func NewTheme(cfg *ThemeConfig) *Theme {
 	if cfg == nil {
-		defaultTheme := &Theme{
-			General: ThemeGeneral{
-				BaseColor: lipgloss.AdaptiveColor{
-					Light: catppuccin.Latte.Base().Hex,
-					Dark:  catppuccin.Mocha.Base().Hex,
-				},
-				PrimaryColor: lipgloss.AdaptiveColor{
-					Light: catppuccin.Latte.Sapphire().Hex,
-					Dark:  catppuccin.Mocha.Sapphire().Hex,
-				},
-				SecondaryColor: lipgloss.AdaptiveColor{
-					Light: catppuccin.Latte.Rosewater().Hex,
-					Dark:  catppuccin.Mocha.Rosewater().Hex,
-				},
-			},
-		}
-		return defaultTheme
+		return NewDefaultTheme()
 	}
 
 	configTheme := &Theme{
 		General: cfg.General,
 	}
 	return configTheme
+}
+
+func NewDefaultTheme() *Theme {
+	defaultTheme := &Theme{
+		General: ThemeGeneral{
+			BaseColor: lipgloss.AdaptiveColor{
+				Light: catppuccin.Latte.Base().Hex,
+				Dark:  catppuccin.Mocha.Base().Hex,
+			},
+			PrimaryColor: lipgloss.AdaptiveColor{
+				Light: catppuccin.Latte.Sapphire().Hex,
+				Dark:  catppuccin.Mocha.Sapphire().Hex,
+			},
+			SecondaryColor: lipgloss.AdaptiveColor{
+				Light: catppuccin.Latte.Rosewater().Hex,
+				Dark:  catppuccin.Mocha.Rosewater().Hex,
+			},
+		},
+	}
+	return defaultTheme
 }
